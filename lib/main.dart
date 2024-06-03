eb7221283aa423c1befa3b94a66dac98f10642e5
JeHeeYu <sees1234558@gmail.com>
2024-06-03 22:18:56 +0900

    feat: 네이버 지도 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:care_route/views/pages/login_page.dart';
 import 'package:care_route/views/pages/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
@@ -9,6 +10,10 @@ import 'app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   KakaoSdk.init(nativeAppKey: '2009513e88266b07a15ad4578d2eacee');
+  await NaverMapSdk.instance.initialize(
+    clientId: 'b8fgmkfu11',
+    //FonAuthFailed: (ex) =>
+  );
   runApp(const MyApp());
 }
 
@@ -24,7 +29,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const LoginPage(),
+        home: const App(),
       ),
     );
   }
--- lib/views/pages/route_guide_page.dart
+++ lib/views/pages/route_guide_page.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:geolocator/geolocator.dart';
 
 class RouteGuidePage extends StatefulWidget {
   const RouteGuidePage({super.key});
@@ -9,8 +14,79 @@ class RouteGuidePage extends StatefulWidget {
 }
 
 class _RouteGuidePageState extends State<RouteGuidePage> {
+  late NaverMapController _mapController;
+  final Completer<NaverMapController> _mapControllerCompleter = Completer();
+  late Position _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentLocation().then((position) {
+      setState(() {
+        _currentPosition = position;
+      });
+    });
+  }
+
+  Future<Position> getCurrentLocation() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    return position;
+  }
+
+  Widget _loadNaverMap() {
+    return FutureBuilder(
+      future: getCurrentLocation(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          _currentPosition = snapshot.data;
+          return NaverMap(
+            options: NaverMapViewOptions(
+              indoorEnable: true,
+              locationButtonEnable: false,
+              consumeSymbolTapEvents: false,
+              locale: const Locale('ko'),
+              logoClickEnable: false,
+              // initialCameraPosition: NCameraPosition(
+              //   target: NLatLng(
+              //   _currentPosition.latitude,
+              //   _currentPosition.longitude),
+              //   zoom: 10,
+              //   bearing: 0,
+              //   tilt: 0,
+              // ),
+            ),
+            onMapReady: (controller) async {
+              _mapController = controller;
+              _mapControllerCompleter.complete(controller);
+
+              // final markerImage =
+              //     await NOverlayImage.fromAssetImage(Images.marker);
+
+              // final marker = NMarker(
+              //     id: '1',
+              //     position: NLatLng(37.5666, 126.979),
+              //     icon: markerImage);
+
+              // _mapController.addOverlay(marker);
+            },
+          );
+        }
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _loadNaverMap(),
+    );
   }
 }
